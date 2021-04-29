4611b61d29f0eb095c5e37939e914464ad621ad5
OlgaJukova <OlgaJukova3787@yandex.ru>
2021-04-29 23:13:59 +0300

    Edit 1

---
+++ Snake/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public  int x;
+        public  int y;
+        public  char sym;
+
+        public Point (int x1, int y1, char sym1 )
+        {
+            x = x1;
+            y = y1;
+            sym = sym1;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
