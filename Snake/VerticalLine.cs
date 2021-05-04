c8e0e4e680b3a8f41a0be0ea2f414ff07730c7e5
OlgaJukova <OlgaJukova3787@yandex.ru>
2021-05-04 22:05:40 +0300

    Added classes Lines

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine: Figure
+    {
+        
+
+        public VerticalLine(int x, int yLow, int yTop, char sym)
+        {
+            plist = new List<Point>();
+            for (int y=yLow; y<yTop; y++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+
+        }
+
+      
+
+    }
+}
